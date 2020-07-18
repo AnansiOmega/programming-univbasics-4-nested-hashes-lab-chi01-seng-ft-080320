d46981e2d6dcace2684dc0d8487afcf515b1f734
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-18 21:17:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
- new_language = "Assembly"
+
 programmer_hash[:dennis_ritchie][:languages][1] = ["Assembly"]
 return programmer_hash
 end
